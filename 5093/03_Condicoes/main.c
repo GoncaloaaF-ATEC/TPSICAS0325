749f3bf85921a0122939de4dd03ae2f3eb759bea
Gonçalo <goncaloaaf@gmail.com>
2025-05-20 16:59:41 +0100

    intro condições

---
+++ 5093/03_Condicoes/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void) {
+    // linha 1
+    // linha 2
+
+    //linha 3.1 ou linha 3.2
+
+
+    // linha n
+
+
+
+
+
+
+
+/*
+ *
+ * >
+ * <
+ * >=
+ * <=
+ * ==
+ * !=
+ * ! - negação
+ *
+ */
+    int num1 = 50;
+    int num2 = 20;
+
+
+    if ( num1 > num2) {
+        printf("Bloco do if");
+        // printf("%d is greater than %d", num1, num2);
+    }else {
+
+        printf("Bloco do else");
+
+    }
+
+    // faça um programas que receba 2 num e diga qual é o maior
+
+
+    
+
+    return 0;
+}
